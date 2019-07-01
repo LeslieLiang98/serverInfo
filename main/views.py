66c7075b1f891440a302f79a67eb97245c602ffa
leslieliang <lesliezwl@hotmail.com>
2019-07-01 23:57:45 +0800

    update

--- main/views.py
+++ main/views.py
@@ -7,7 +7,4 @@ from snmp_cmds import snmpwalk
 class Index(View):
 	# 首页
 	def get(self, request):
-		serverDesc = snmpwalk(ipaddress = '10.1.1.105', community = 'zwliang98', oid = '.1.3.6.1.2.1.1.1.0')
-		return render(request, 'index.html', {
-			'serverDesc': serverDesc[0][1],
-			})
+		return render(request, 'index.html')
