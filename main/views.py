72422f2e629b0d58fbd12f587e9726b6f6e9fb34
LeslieLiang98 <lesliezwl@hotmail.com>
2019-07-03 18:18:17 +0800

    update

--- main/views.py
+++ main/views.py
@@ -4,6 +4,8 @@ from django.views import View
 from django.http import JsonResponse
 from django.conf import settings
 from snmp_cmds import snmpwalk
+from time import time, strftime, localtime
+import re
 
 HOST = settings.HOST
 COMMUNITY = settings.COMMUNITY
@@ -16,7 +18,22 @@ class GetSystemInfo(View):
 		status = False
 		try:
 			sysUpTime = snmpwalk(ipaddress = HOST, community = COMMUNITY, port = PORT, oid = settings.SYS_UP_TIME)
+			ssCpuUser = snmpwalk(ipaddress = HOST, community = COMMUNITY, port = PORT, oid = settings.SS_CPU_USER)
+			ssCpuSystem = snmpwalk(ipaddress = HOST, community = COMMUNITY, port = PORT, oid = settings.SS_CPU_SYSTEM)
+			ssCputIdle = snmpwalk(ipaddress = HOST, community = COMMUNITY, port = PORT, oid = settings.SS_CPU_IDLE)
+			memTotalReal = snmpwalk(ipaddress = HOST, community = COMMUNITY, port = PORT, oid = settings.MEM_TOTAL_REAL)
+			memAvailReal = snmpwalk(ipaddress = HOST, community = COMMUNITY, port = PORT, oid = settings.MEM_AVAIL_REAL)
+
+			currentTime = strftime('%H:%M:%S', localtime(time()))
+
+			jsonData['currentTime'] = currentTime
 			jsonData['sysUpTime'] = sysUpTime[0][1]
+			jsonData['ssCpuUser'] = int(ssCpuUser[0][1])
+			jsonData['ssCpuSystem'] = int(ssCpuSystem[0][1])
+			jsonData['ssCpuIdle'] = int(ssCputIdle[0][1])
+			jsonData['memTotalReal'] = round(int(re.findall('^[1-9][0-9]+', memTotalReal[0][1])[0]) / 1024)
+			jsonData['memAvailReal'] = round(int(re.findall('^[1-9][0-9]+', memAvailReal[0][1])[0]) / 1024)
+			jsonData['memTotalFree'] = jsonData['memTotalReal'] - jsonData['memAvailReal']
 			status = True
 		except Exception as error:
 			pass
--- static/js/basic.js
+++ static/js/basic.js
@@ -7,8 +7,58 @@ layui.use([], function(){
 			delimiters: ['${', '}'],
 		});
 
-	var timer = setInterval(getSystemInfo, 1000);
+	var timer = setInterval(getSystemInfo, 3000);
 
+	var chartXData = [];
+	var charSeries0Data = [];
+	var charSeries1Data = [];
+	var charSeries2Data = [];
+	var cpuUserChart = Highcharts.chart('cpuUseInfo', {
+		chart: {
+			width: 700,
+		},
+		title: {text: 'CPU使用百分比'},
+		credits: {enabled: false},
+		yAxis: {title: {text: 'CPU使用百分比'}, ceiling: 100, max: 100,},
+		xAxis: {
+			categories: chartXData,
+		},
+		tooltip: {valueSuffix: '%'},
+		series: [{
+			name: '用户CPU使用百分比',
+			data: charSeries0Data,
+		},{
+			name: '系统CPU使用百分比',
+			data: charSeries1Data,
+		},{
+			name: '空闲CPU百分比',
+			data: charSeries2Data,
+		}]
+	});
+	var memoryInfoChart = Highcharts.chart('memoryInfo', {
+		chart: {type: 'pie'},
+		title: {text: '内存使用百分比'},
+		credits: {enabled: false},
+		tooltip: {valueSuffix: '%'},	
+		plotOptions: {
+			pie: {
+				dataLabels: {
+					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+				},
+			},
+		},
+		series: [{
+			cursor: 'pointer',
+			name: '内存占比',
+			data: [{
+				name: '空闲内存',
+			},{
+				name: '已使用内存',
+			}]
+		}]
+	});
+
+	getSystemInfo();
 	function getSystemInfo(){
 		// 获取系统基础信息
 		$.ajax({
@@ -17,7 +67,42 @@ layui.use([], function(){
 			dataType: 'json',
 			data: {csrfmiddlewaretoken: token},
 			success: function(result){
-				systemApp.sysUpTime = result.sysUpTime;
+				var sysUpTime = result.sysUpTime.split(':');
+				var sysUpTime_day = sysUpTime[0];
+				var sysUpTime_hour = sysUpTime[1];
+				var sysUpTime_minute = sysUpTime[2];
+				var sysUpTime_second = sysUpTime[3];
+				systemApp.sysUpTime = sysUpTime_day + '天' + sysUpTime_hour + '小时' + sysUpTime_minute + '分钟' + sysUpTime_second + '秒';
+
+				chartXData.push(result.currentTime);
+				charSeries0Data.push(result.ssCpuUser);
+				charSeries1Data.push(result.ssCpuSystem);
+				charSeries2Data.push(result.ssCpuIdle);
+
+				shift = chartXData.length > 10;
+				if(shift){
+					chartXData.shift();
+					charSeries0Data.shift();
+					charSeries1Data.shift();
+					charSeries2Data.shift();
+				}
+				cpuUserChart.xAxis[0].update({
+					categories: chartXData,
+				});
+				cpuUserChart.series[0].update({
+					data: charSeries0Data,
+				})
+				cpuUserChart.series[1].update({
+					data: charSeries1Data,
+				})
+				cpuUserChart.series[2].update({
+					data: charSeries2Data,
+				})
+
+				percPoint = result.memTotalReal / 100;
+				memTotalFreePerc = parseFloat((result.memTotalFree / percPoint).toFixed(2));
+				memAvailRealPerc = parseFloat((result.memAvailReal / percPoint).toFixed(2));
+				memoryInfoChart.series[0].setData([memTotalFreePerc, memAvailRealPerc]);
 			},
 			error: function(){
 				var errorMsg = '连接服务器失败';
@@ -27,23 +112,6 @@ layui.use([], function(){
 		});
 	}
 
-	var cpuUserChart = Highcharts.chart('cpuUser', {
-		chart: {
-			type: 'pie',
-		},
-		title: {
-			text: '用户CPU百分比',
-		},
-		tooltip: {
-			valueSuffix: '%'
-		},
-		series: [{
-			type: 'pie',
-			name: 'cpu',
-			data: [
-				['空闲', 100],
-				['已使用', 0.0],
-			]
-		}]
-	});
+	
+
 })
